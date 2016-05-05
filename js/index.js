3c7e08b0c4c002be3269cb7223e7191da7654e2d
Aron Adler <aronadler@hotmail.com>
2016-05-05 16:22:04 +0100

    Made motion use CSS transforms for smoother movement + indented code using htmlprettify

--- js/index.js
+++ js/index.js
@@ -6,76 +6,77 @@ init();
 
 /*** Functions ***/
 function init() {
-  setInterval(checker, 10);
-  score = 0;
-  timer = 0;
-  diff = 1;
-  currSize = origSize;
+    setInterval(checker, 10);
+    score = 0;
+    timer = 0;
+    diff = 1;
+    currSize = origSize;
 }
 
 $("#ball").click(function() {
-  timer = 0;
-  move(this);
+    timer = 0;
+    move(this);
 
-  $(this).css({
-      backgroundColor: "#F22613"
-    }).delay(100 * diff)
-    .queue(function(next) {
-      $(this).css({
-        backgroundColor: "#1F3A93"
-      });
-      next();
-    });
-  /*    $('body').css({
-          backgroundColor: "#9A12B3"
+    $(this).css({
+            backgroundColor: "#F22613"
         }).delay(100 * diff)
         .queue(function(next) {
-          $('body').css({
-            backgroundColor: "#59abe3"
-          });
-          next();
-        });*/
+            $(this).css({
+                backgroundColor: "#1F3A93"
+            });
+            next();
+        });
+    /*    $('body').css({
+            backgroundColor: "#9A12B3"
+          }).delay(100 * diff)
+          .queue(function(next) {
+            $('body').css({
+              backgroundColor: "#59abe3"
+            });
+            next();
+          });*/
 
-  score++;
-  $("#score").text(score);
+    score++;
+    $("#score").text(score);
 
-  diff *= 0.95;
+    diff *= 0.95;
 
-  currSize = 2 * Math.sqrt((diff * (origSize * origSize * Math.PI) / 4) / Math.PI);
-  $(this).height(currSize);
-  $(this).width(currSize);
+    currSize = 2 * Math.sqrt((diff * (origSize * origSize * Math.PI) / 4) / Math.PI);
+    $(this).height(currSize);
+    $(this).width(currSize);
 });
 
 function checker() {
-  timer++;
-  if (timer > 300 * diff) {
-    move("#ball");
-    timer = 0;
-  }
+    timer++;
+    if (timer > 300 * diff) {
+        move("#ball");
+        timer = 0;
+    }
 }
 
 function newCoords() {
-  y = currSize * margin + Math.random() * (height() - currSize - 2 * currSize * margin);
-  x = currSize * margin + Math.random() * (width() - currSize - 2 * currSize * margin);
+    y = currSize * margin + Math.random() * (height() - currSize - 2 * currSize * margin);
+    x = currSize * margin + Math.random() * (width() - currSize - 2 * currSize * margin);
 }
 
 function newLocation(ball) {
-  $(ball).animate({
-      top: y,
-      left: x
-    }, 50 + 100 * diff,
-    'swing');
+    // $(ball).animate({
+    //     top: y,
+    //     left: x
+    //   }, 50 + 100 * diff,
+    //   'swing');
+    $(ball).css({ "transform": "translate(" + x + "px, " + y + "px)" });
 }
 
 function move(ball) {
-  newCoords();
-  newLocation(ball);
+    newCoords();
+    newLocation(ball);
 }
 
 function height() {
-  return window.innerHeight;
+    return window.innerHeight;
 }
 
 function width() {
-  return window.innerWidth;
+    return window.innerWidth;
 }
